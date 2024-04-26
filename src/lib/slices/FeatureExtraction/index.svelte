6bde31f30ba866514ec71ea71e97ad2cc83856a9
Ishaan <ishaankumar1111@gmail.com>
2024-04-26 23:33:03 +0530

    Record , Pause , Stop button created more Efficiently

--- src/lib/slices/FeatureExtraction/index.svelte
+++ src/lib/slices/FeatureExtraction/index.svelte
@@ -2,12 +2,21 @@
 	import Bounded from "$lib/components/Bounded.svelte";
 	import StarGrid from "$lib/components/StarGrid.svelte";
 	import { PrismicRichText } from "@prismicio/svelte";
+	import {onMount} from "svelte"
 
 	/** @type {import("@prismicio/client").Content.FeatureExtractionSlice} */
 	export let slice;
 
+	onMount(()=>{
 
-
+		const Record_button = document.querySelector("#Record")
+		const Pause_button = document.querySelector("#Pause")
+		const sause_button = document.querySelector("#Stop")
+		
+		
+		
+		
+	})
 </script>
 
 <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
@@ -27,13 +36,18 @@
 		</div>
 		
 		<div class="glass-container m-6 p-10">
-			{#each slice.items as item (item.button_label)}
 
-			<button class="m-4 p-4 px-8 border-2 rounded-full max-w-3xl text-balance ">
-				<PrismicRichText field={item.button_label} />
-				
+			<button id="Record" class="m-4 p-4 px-8 border-2 rounded-full max-w-3xl text-balance text-black bg-green-500 font-medium scale-1 hover:scale-[1.15] transition duration-300 ease-in-out"> 
+				<PrismicRichText field={slice.items[0].button_label} />	
 			</button>
 			
-			{/each}
+			<button id="Pause" class="m-4 p-4 px-8 border-2 rounded-full max-w-3xl text-balance text-black bg-yellow-500 font-medium scale-1 hover:scale-[1.15] transition duration-300 ease-in-out "> 
+				<PrismicRichText field={slice.items[1].button_label} />	
+			</button>
+			
+			<button id="Stop" class="m-4 p-4 px-8 border-2 rounded-full max-w-3xl text-balance text-white bg-red-600 font-medium scale-1 hover:scale-[1.15] transition duration-300 ease-in-out"> 
+				<PrismicRichText field={slice.items[2].button_label} />	
+			</button>
+			
 		</div>
 </Bounded>
